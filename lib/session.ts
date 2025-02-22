e632bc59b6a30f952185ea6a076e8ee24fa9dfdf
VishnuSM9095 <suganthivisnu666@gmail.com>
2025-02-22 22:47:05 +0530

    lets see

--- lib/session.ts
+++ lib/session.ts
@@ -1,9 +1,7 @@
-// import { getServerSession } from "next-auth/next"
+import { getServerSession } from "next-auth/next"
 
-// import { authOptions } from "@/lib/auth"
 
-// export async function getCurrentUser() {
-//   const session = await getServerSession(authOptions)
-
-//   return session?.user
-// }
+export async function getCurrentUser() {
+ 
+  return null;
+}
